79ec949e90d9d4d0a1b6dce944334f7eac451649
skabir8 <shariar.k8@gmail.com>
2020-04-05 18:27:40 -0400

    add app listener on port 3000

--- server/app.js
+++ server/app.js
@@ -1,3 +1,6 @@
+
+require('dotenv').config()
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -5,8 +8,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var models, { sequelize } = require('./models');
 
 var app = express();
+const port = 3000
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 module.exports = app;
