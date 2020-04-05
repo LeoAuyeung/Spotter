848c35d37074748cfc0733285b2ea5edb9f2b15c
skabir8 <shariar.k8@gmail.com>
2020-04-05 19:20:50 -0400

    modify db to include base sequalize

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,12 @@
-var fs = require('fs');
-var path = require('path');
-var Sequelize = require('sequelize');
-var configJson = require('../config/config');
-
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var configJson = require("../config/config");
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
 
 const config = configJson[env];
-
-console.log('this is the environment: ', env);
 
 const db = {};
 
@@ -18,32 +15,40 @@ if (true) {
   sequelize = new Sequelize(
     process.env.DATABASE_NAME,
     process.env.DATABASE_USER,
-    process.env.DATABASE_PASSWORD, {
+    process.env.DATABASE_PASSWORD,
+    {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
-      dialect: 'postgres',
+      dialect: "postgres",
       dialectOption: {
         ssl: true,
-        native: true
+        native: true,
       },
-      logging: true
+      logging: true,
+      define: {
+        timestamps: false
+    }
     }
   );
 } else {
   sequelize = new Sequelize(
-     config.database, config.username, config.password, config
+    config.database,
+    config.username,
+    config.password,
+    config
   );
 }
 
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter((file) => {
-    return (file.indexOf('.') !== 0) && 
-           (file !== basename) && (file.slice(-3) === '.js');
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
   .forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
+    console.log(db[model.name]);
   });
 
 Object.keys(db).forEach((modelName) => {
@@ -52,6 +57,6 @@ Object.keys(db).forEach((modelName) => {
   }
 });
 
+db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-
 module.exports = db;
