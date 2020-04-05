b67c491c3b53bd419b9d8e37b7b8b60fd4c23f5d
skabir8 <shariar.k8@gmail.com>
2020-04-05 19:21:25 -0400

    modify users model

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const userController = require("../controllers/users");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.route("/").get(userController.getAllUsers);
 
-router.post('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
 module.exports = router;
