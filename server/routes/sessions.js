4ae977d2ca092637061024b77d2b43d919658bbb
skabir8 <shariar.k8@gmail.com>
2020-04-21 04:13:34 -0400

    add delete route for session fxn

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -7,5 +7,6 @@ const sessionsController = require("../controllers/sessions");
 router.route("/").get(sessionsController.getAllSessions);
 router.route("/").post(auth, sessionsController.createSession);
 router.route("/:id").put(auth, sessionsController.editSession);
+router.route("/:id").delete(auth, sessionsController.deleteSession);
 
 module.exports = router;
