e274a152b00c285609aed7c544fd2e2a29bec99a
skabir8 <shariar.k8@gmail.com>
2020-04-21 02:26:26 -0400

    add new route for decode jwt

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,7 @@ var auth = require('../middleware/auth');
 const userController = require("../controllers/users");
 
 router.route("/").get(auth, userController.getAllUsers);
+router.route("/auth/decodeJwt").get(userController.decodeJwtToken);
 router.route("/auth/login").post(userController.loginUser);
 router.route("/auth/register").post(userController.registerUser);
 
