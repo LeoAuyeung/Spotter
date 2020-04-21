6127aa00c8ebba21f7b2bd5ae778704e054789c9
skabir8 <shariar.k8@gmail.com>
2020-04-21 04:05:42 -0400

    create editSession for PUT session route with id

--- server/controllers/sessions.js
+++ server/controllers/sessions.js
@@ -31,15 +31,36 @@ async function createSession(req, res, next) {
         res.status(201).json(newSession);
 	} catch (err) {
 		console.log(err);
-		res.status(401).json({ code: "error", message: "Error with creating Session. Please retry." });
+		return res.status(401).json({ code: "error", message: "Error with creating Session. Please retry." });
 	}
 }
 
 async function editSession(req, res, next) {
 	try {
+        const { id } = req.params;
+        const currentSession = await database.sessions.findOne({  raw: true , where: {id: id} });
+        if (currentSession == null){
+            return res.status(401).json({ code: "error", message: "Session does not exist." });
+        }
+        let newSession = req.body;
+        
+        let decodedJwt = await decodeJwt(req.headers);
+        let currentUser = await database.users.findOne({  raw: true , where: {email: decodedJwt.email} })
+
+        if ((Number(currentUser.id) != currentSession.ownerId)){
+            return res.status(401).json({ code: "error", message: "Session does not exist." });
+        }
+        if (req.body.id && (Number(req.body.id) != currentSession.ownerId)){
+            return res.status(401).json({ code: "error", message: "Unathorized to edit." });
+        }
+        const [ updated ] = await database.sessions.update(req.body, {where: { id: id }});
+        if (updated) {
+            const updatedSessions = await database.sessions.findOne({ where: { id: id } });
+            return res.status(200).json({ updatedSessions });
+        }
 	} catch (err) {
         console.log(err);
-        res.status(401).json({ code: "error", message: "Error with updating Session. Please retry." });
+        return res.status(401).json({ code: "error", message: "Error with updating Session. Please retry." });
 	}
 }
 
