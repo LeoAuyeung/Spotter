cb8c1829f4d879d1f03735293beef18d7ff1a943
skabir8 <shariar.k8@gmail.com>
2020-04-20 23:48:26 -0400

    add required jwt module in app file

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const apiRouter = require("./routes/index");
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
 
 app.use("/api", apiRouter);
