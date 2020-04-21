313886e625da58c4c13e222062f488be8ef66ec5
skabir8 <shariar.k8@gmail.com>
2020-04-21 02:08:36 -0400

    add route for login and registration

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,5 +6,6 @@ const userController = require("../controllers/users");
 
 router.route("/").get(auth, userController.getAllUsers);
 router.route("/auth/login").post(userController.loginUser);
+router.route("/auth/register").post(userController.registerUser);
 
 module.exports = router;
