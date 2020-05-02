9f23614c125577cd4f20bd4e630f0144d15b069c
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-05-01 22:20:46 -0400

    added proxy, added backend url to an env file

--- client/src/components/containers/SigninContainer.jsx
+++ client/src/components/containers/SigninContainer.jsx
@@ -3,44 +3,44 @@ import { connect } from "react-redux";
 import { SigninView } from "../views";
 
 class SigninContainer extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
-		this.state={
+		this.state = {
 			email: "",
 			password: ""
 		}
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
-	 // EVENT HANDLERS
-	 handleChange = async(e) => {
-		 e.preventDefault();
+	// EVENT HANDLERS
+	handleChange = async (e) => {
+		e.preventDefault();
 		this.setState({ [e.target.name]: e.target.value });
-	  };
+	};
 
-	  handleSubmit = async(e) => {
+	handleSubmit = async (e) => {
 		// this is only if the user deletes the initial properties and leaves fields blank
 		// Its okay if save changes is clicked and nothing actually changed
 		e.preventDefault();
-		
-		  // send changed user data
-		  let user = {
+
+		// send changed user data
+		let user = {
 			// NOT changed but need it for Link
 			email: this.state.email,
 			password: this.state.password
-		  };
-	
-		  // send to edit User to update User AND the database
-		  console.log(user)
-	  };
+		};
+
+		// send to edit User to update User AND the database
+		console.log(user)
+	};
 
 	render() {
-		return <SigninView 
-		handleChange = {this.handleChange}
-		handleSubmit = {this.handleSubmit}
-		email = {this.state.email}
-		password = {this.state.password}
-        />;
+		return <SigninView
+			handleChange={this.handleChange}
+			handleSubmit={this.handleSubmit}
+			email={this.state.email}
+			password={this.state.password}
+		/>;
 	}
 }
 
