2f5d24d034c1fa84e6edb4452246d8fe514f5480
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-05-04 02:32:07 -0400

    remove call to www

--- server/app.js
+++ server/app.js
@@ -3,27 +3,29 @@ if (process.env.NODE_ENV !== "production") {
 	require("dotenv").config();
 }
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const bodyParser = require('body-parser');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const bodyParser = require("body-parser");
 
 const apiRouter = require("./routes/index");
 
 const PORT = process.env.PORT;
 
 var app = express();
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => res.send("Spotter Backend"))
+app.get("/", (req, res, next) => res.send("Spotter Backend"));
 app.use("/api", apiRouter);
 
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
+app.listen(PORT, () =>
+	console.log(`Example app listening at http://localhost:${PORT}`)
+);
 
 module.exports = app;
