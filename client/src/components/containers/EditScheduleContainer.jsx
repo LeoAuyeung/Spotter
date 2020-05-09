3d8b74d009e14dc4d2644614d67e1e237ef9ffab
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2020-05-09 19:09:00 -0400

    changing edit schedule form

--- client/src/components/containers/EditScheduleContainer.jsx
+++ client/src/components/containers/EditScheduleContainer.jsx
@@ -11,40 +11,9 @@ class EditScheduleContainer extends Component {
             columns: [{ name: "day", title: "Day" },
             { name: "startTime", title: "Start Time" },
             { name: "endTime", title: "EndTime" }],
-            rows: 1,
-            setRows: []
+ 
 
         }
-        this.commitChanges = this.commitChanges.bind(this)
-        // this.getRowId = this.getRowId.bind(this)
-    }
-
-    // getRowId = (row) => {row.id};
-
-   commitChanges = ({ added, changed, deleted }) => {
-    let changedRows;
-        if (added) {
-        const startingAddedId =
-            this.state.rows.length > 0 ? this.state.rows[this.state.rows.length - 1].id + 1 : 0;
-        changedRows = [
-            ...this.state.rows,
-            ...added.map((row, index) => ({
-            id: startingAddedId + index,
-            ...row
-            }))
-        ];
-        }
-        if (changed) {
-        changedRows = this.state.rows.map(row =>
-            changed[row.id] ? { ...row, ...changed[row.id] } : row
-        );
-        }
-        if (deleted) {
-        const deletedSet = new Set(deleted);
-        changedRows = this.state.rows.filter(row => !deletedSet.has(row.id));
-        }
-        this.state.setRows(changedRows)
-
     }
 
 
@@ -53,9 +22,6 @@ class EditScheduleContainer extends Component {
         return <EditScheduleView 
         rows = {this.state.rows}
         columns = {this.state.columns}
-        // getRowId = {this.getRowId}
-        commitChanges = {this.commitChanges}
-
 		
 		/>;
 		
