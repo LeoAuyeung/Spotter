12ef4cc2813fdc2c5d6f4ecf9eabb636367525b5
LeoAuyeung <leo297747@gmail.com>
2020-05-12 05:56:16 -0400

    Add: get user's connections, create connection between users, delete connection by id

--- server/controllers/connections.js
+++ server/controllers/connections.js
@@ -1,7 +1,12 @@
 var database = require('../models');
+var decodeJwt = require('../middleware/decodeJwt');
+const { Op } = require("sequelize");
 
 const connectionsController = {
 	getAllConnections: getAllConnections,
+	getUserConnections: getUserConnections,
+	createConnection: createConnection,
+	deleteConnection: deleteConnection,
 };
 
 async function getAllConnections(req, res, next) {
@@ -11,6 +16,69 @@ async function getAllConnections(req, res, next) {
     }
     catch (err) {
 		console.log(err);
+	}
+}
+
+async function getUserConnections(req, res, next) {
+	try {
+        let decodedJwt = await decodeJwt(req.headers);
+		let currentUser = await database.users.findOne({ raw: true , where: { email: decodedJwt.email } });
+
+		// Get all connections of current user
+		const userConnections = await database.connections.findAll(({ raw: true , where: { 
+			[Op.or]: [
+				{ userId_1: currentUser.id,  },
+				{ userId_2: currentUser.id,  }
+			]
+		}}));
+
+		res.status(200).json(userConnections);
+    }
+    catch (err) {
+		console.log(err);
+	}
+}
+
+async function createConnection(req, res, next) {
+	try {
+		const { userId } = req.params; // Id of user2, user that you are connecting to
+		
+		const allConnections = await database.connections.findAll();
+		const latestConnection = allConnections[allConnections.length-1];
+		const latestInt = parseInt(latestConnection.int);
+		let newConnection = { "int": String(latestInt + 1) };
+		
+        let decodedJwt = await decodeJwt(req.headers);
+        let currentUser = await database.users.findOne({  raw: true , where: {email: decodedJwt.email} })
+        newConnection.userId_1 = currentUser.id;
+		newConnection.userId_2 = userId;
+		
+        await database.connections.create(newConnection);
+        res.status(201).json(newConnection);
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ code: "error", message: "Error with creating Session. Please retry." });
+	}
+}
+
+async function deleteConnection(req, res, next) {
+	try {
+        // Using Connection unique id to check whether it exists and is available to delete
+        const { id } = req.params; // Connection unique id
+        const currentConnection = await database.connections.findOne({ raw: true , where: { int: id } });
+        if (currentConnection == null) {
+            return res.status(401).json({ code: "error", message: "Connection does not exist." });
+        }
+        
+        // Delete the Connection
+        const deleted = await database.connections.destroy({ where: { int: id } });
+        if (deleted) {
+            return res.status(201).json({ code: "success", message: "Connection successfully deleted." });
+        }
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json({ code: "error", message: "Error with deleting Connection. Please retry." });
 	}
 }
 
--- server/routes/connections.js
+++ server/routes/connections.js
@@ -5,5 +5,8 @@ var auth = require('../middleware/auth');
 const connectionsController = require("../controllers/connections");
 
 router.route("/").get(connectionsController.getAllConnections);
+router.route("/:userId").get(connectionsController.getUserConnections);
+router.route("/:userId").post(connectionsController.createConnection);
+router.route("/:id").delete(connectionsController.deleteConnection);
 
 module.exports = router;
