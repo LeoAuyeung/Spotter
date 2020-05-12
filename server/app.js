7e4ec8743d6b8aad5c3a4cb2e799c4870d8ae54e
angelalim1010 <angela.lim57@myhunter.cuny.edu>
2020-05-12 15:20:29 -0400

    adding heroku post build scripts

--- server/app.js
+++ server/app.js
@@ -22,4 +22,14 @@ app.use(bodyParser.json());
 app.get("/", (req, res, next) => res.send("Spotter Backend"));
 app.use("/api", apiRouter);
 
+// Heroku post-build script
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+	  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+  }
+
 module.exports = app;
