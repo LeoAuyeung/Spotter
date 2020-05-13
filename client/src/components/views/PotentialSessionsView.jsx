a06b5e2f70d23d8fbf7985ccaa193c3199cfba58
skabir8 <shariar.k8@gmail.com>
2020-05-13 12:41:10 -0400

    add new date

--- client/src/components/views/PotentialSessionsView.jsx
+++ client/src/components/views/PotentialSessionsView.jsx
@@ -3,6 +3,7 @@ import Avatar from "@material-ui/core/Avatar";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
+var getNearestDayOfWeek = require('get-nearest-day-of-week')
 
 const avatarStyle = {
 	width: "100px",
@@ -12,6 +13,26 @@ const avatarStyle = {
 
 const PotentialSessionsView = (props) => {
 	const { sessions } = props;
+	const currDate = new  Date()
+	var add7Days = currDate
+	add7Days.setDate(add7Days.getDate() + 7);
+
+	const monday = getNearestDayOfWeek(currDate, 1).format("YYYY-MM-DD")  >= currDate ? getNearestDayOfWeek(currDate, 1).format("YYYY-MM-DD") : getNearestDayOfWeek(add7Days, 1).format("YYYY-MM-DD")
+	const tuesday = getNearestDayOfWeek(currDate, 2).format("YYYY-MM-DD")  >= currDate ? getNearestDayOfWeek(currDate, 2).format("YYYY-MM-DD") : getNearestDayOfWeek(add7Days, 2).format("YYYY-MM-DD")
+	const wednesday = getNearestDayOfWeek(currDate, 3).format("YYYY-MM-DD")  >= currDate ? getNearestDayOfWeek(currDate, 3).format("YYYY-MM-DD") : getNearestDayOfWeek(add7Days, 3).format("YYYY-MM-DD")
+	const thursday = getNearestDayOfWeek(currDate, 4).format("YYYY-MM-DD")  >= currDate ? getNearestDayOfWeek(currDate, 4).format("YYYY-MM-DD") : getNearestDayOfWeek(add7Days, 4).format("YYYY-MM-DD")
+	const friday = getNearestDayOfWeek(currDate, 5).format("YYYY-MM-DD")  >= currDate ? getNearestDayOfWeek(currDate, 5).format("YYYY-MM-DD") : getNearestDayOfWeek(add7Days, 5).format("YYYY-MM-DD")
+	const saturday = getNearestDayOfWeek(currDate, 6).format("YYYY-MM-DD")  >= currDate ? getNearestDayOfWeek(currDate, 6).format("YYYY-MM-DD") : getNearestDayOfWeek(add7Days, 6).format("YYYY-MM-DD")
+	const sunday = getNearestDayOfWeek(currDate, 7).format("YYYY-MM-DD")  >= currDate ? getNearestDayOfWeek(currDate, 7).format("YYYY-MM-DD") : getNearestDayOfWeek(add7Days, 7).format("YYYY-MM-DD")
+	const dateMap = {
+		1: sunday,
+		2: monday,
+		3: tuesday,
+		4: wednesday,
+		5: thursday,
+		6: friday,
+		7: saturday
+	}
 	return (
 		<div>
 			{/* {sessions} */}
