228f395294ccc4984c33fd07a1c10d64f5d6cbdd
kyu21 <kun.yu25@myhunter.cuny.edu>
2020-05-13 09:16:54 -0400

    home

--- client/src/components/views/HomeView.jsx
+++ client/src/components/views/HomeView.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Home.css";
+
 const HomeView = (props) => {
-	return <div></div>;
+	const photos = [
+		"https://caliberstrong.com/wp-content/uploads/2020/04/bench-press-spot.jpg",
+		"https://athleticperformancetc.files.wordpress.com/2015/02/adv_benchpress_031.jpg",
+		"https://www.muscleandfitness.com/wp-content/uploads/2018/03/1109-lifting-bench-press-spotter.jpg?w=1109",
+		"https://upload.wikimedia.org/wikipedia/commons/a/aa/Bench_press_1.jpg",
+	];
+
+	const imgs = photos.map((p) => <img src={p}></img>);
+
+	return <div className="container">{imgs}</div>;
 };
 
 export default HomeView;
