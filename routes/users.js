49b7ef685218b4e18b6182d511e472dd9fcd2287
skabir8 <shariar.k8@gmail.com>
2020-05-13 04:21:26 -0400

    other profile

--- routes/users.js
+++ routes/users.js
@@ -11,5 +11,6 @@ router.route("/auth/login").post(userController.loginUser);
 router.route("/auth/register").post(userController.registerUser);
 router.route("/me").get(userController.me);
 router.route("/profile").get(userController.profile);
+router.route("/profile/:id").get(userController.otherProfile);
 
 module.exports = router;
